9c4184b7706914572d28acfd989f1dee32259eb4
Ville Kopio <ville.kopio@kodan.fi>
2021-02-24 15:55:30 +0200

    Cleanup and refactor.

--- assignment_2/src/DL_hw2.py
+++ assignment_2/src/DL_hw2.py
@@ -9,38 +9,34 @@ import numpy as np
 
 from torchvision import transforms, datasets
 
-#--- hyperparameters ---
+NUM_CLASSES = 24
+DATA_DIR = '../data/sign_mnist_%s'
+
 N_EPOCHS = 10
 BATCH_SIZE_TRAIN = 100
 BATCH_SIZE_TEST = 100
 LEARNING_RATE = 0.01
 
 
-#--- fixed constants ---
-NUM_CLASSES = 24
-DATA_DIR = '../data/sign_mnist_%s'
+def load_dataset(dataset_name, shuffle=False, extra_transforms=[]):
+    common_transforms = [
+        transforms.ToTensor()
+    ]
 
-# --- Dataset initialization ---
+    dataset = datasets.ImageFolder(
+        DATA_DIR % dataset_name,
+        transform=transforms.Compose(common_transforms + extra_transforms)
+    )
 
-# We transform image files' contents to tensors
-# Plus, we can add random transformations to the training data if we like
-# Think on what kind of transformations may be meaningful for this data.
-# Eg., horizontal-flip is definitely a bad idea for sign language data.
-# You can use another transformation here if you find a better one.
-train_transform = transforms.Compose([
-                                        #transforms.RandomHorizontalFlip(),
-                                        transforms.ToTensor()])
-test_transform = transforms.Compose([transforms.ToTensor()])
+    data_loader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE_TRAIN,
+        shuffle=shuffle
+    )
 
-train_set = datasets.ImageFolder(DATA_DIR % 'train', transform=train_transform)
-dev_set   = datasets.ImageFolder(DATA_DIR % 'dev',   transform=test_transform)
-test_set  = datasets.ImageFolder(DATA_DIR % 'test',  transform=test_transform)
+    return data_loader
 
-# Create Pytorch data loaders
-train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=BATCH_SIZE_TEST, shuffle=False)
 
-#--- model ---
 class CNN(nn.Module):
     def __init__(self, num_classes=NUM_CLASSES):
         super(CNN, self).__init__()
@@ -60,67 +56,78 @@ class CNN(nn.Module):
         x = self.fc3(x)
         return x
 
-#--- set up ---
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
 
+train_loader = load_dataset('train', shuffle=True, extra_transforms=[])
+dev_loader = load_dataset('dev')
+test_loader = load_dataset('test')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = CNN().to(device)
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
 loss_function = nn.CrossEntropyLoss()
 
-#--- training ---
-for epoch in range(N_EPOCHS):
-    train_loss = 0
-    train_correct = 0
+
+def train():
+    for epoch in range(N_EPOCHS):
+        train_loss = 0
+        train_correct = 0
+        total = 0
+
+        for batch_num, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+
+            prediction = model(data)
+            output = loss_function(prediction, target)
+
+            train_loss += output.data.item()
+            _, predicted = torch.max(prediction, 1)
+            train_correct += (predicted == target).sum().item()
+            total += target.size(0)
+
+            output.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            print('Training: Epoch %d/%d | Batch %d/%d | Loss: %.4f | Train Acc: %.3f%% (%d/%d)' %
+                  (epoch + 1,
+                   N_EPOCHS,
+                   batch_num,
+                   len(train_loader),
+                   train_loss / (batch_num + 1),
+                   100. * train_correct / total,
+                   train_correct,
+                   total),
+                  end="\r",
+                  flush=True)
+
+        # Please implement early stopping here.
+
+
+def test():
+    test_loss = 0
+    test_correct = 0
     total = 0
 
-    for batch_num, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    with torch.no_grad():
+        for batch_num, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
 
-        prediction = model(data)
-        output = loss_function(prediction, target)
+            prediction = model(data)
+            _, predicted = torch.max(prediction.data, 1)
 
-        train_loss += output.data.item()
-        _, predicted = torch.max(prediction, 1)
-        train_correct += (predicted == target).sum().item()
-        total += target.size(0)
+            total += target.size(0)
+            test_correct += (predicted == target).sum()
+            test_loss += loss_function(prediction, target)
 
-        output.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+            print('Evaluating: Batch %d/%d: Loss: %.4f | Test Acc: %.3f%% (%d/%d)' %
+                  (batch_num + 1,
+                   len(test_loader),
+                   test_loss / (batch_num + 1),
+                   100. * test_correct / total,
+                   test_correct,
+                   total))
 
-        print(f'Training: Epoch {epoch+1}/{N_EPOCHS} | Batch {batch_num}/{len(train_loader)} | Loss: %.4f | Train Acc: %.3f%% ({train_correct}/{total})' %
-              (train_loss / (batch_num + 1), 100. * train_correct / total),
-               end="\r",
-               flush=True)
 
-    # WRITE CODE HERE
-    # Please implement early stopping here.
-    # You can try different versions, simplest way is to calculate the dev error and
-    # compare this with the previous dev error, stopping if the error has grown.
-
-#--- test ---
-test_loss = 0
-test_correct = 0
-total = 0
-
-with torch.no_grad():
-    for batch_num, (data, target) in enumerate(test_loader):
-        data, target = data.to(device), target.to(device)
-
-        prediction = model(data)
-        _, predicted = torch.max(prediction.data, 1)
-
-        total += target.size(0)
-        test_correct += (predicted == target).sum()
-        test_loss += loss_function(prediction, target)
-
-        print('Evaluating: Batch %d/%d: Loss: %.4f | Test Acc: %.3f%% (%d/%d)' % 
-              (batch_num + 1, 
-               len(test_loader), 
-               test_loss / (batch_num + 1),
-               100. * test_correct / total, 
-               test_correct, 
-               total))
+if __name__ == "__main__":
+    train()
+    test()
