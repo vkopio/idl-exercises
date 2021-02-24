b0beab3b4ab3790a050bdc8ba590bf87153282da
Ville Kopio <ville.kopio@kodan.fi>
2021-02-24 15:07:41 +0200

    Initial model.

--- assignment_2/src/DL_hw2.py
+++ assignment_2/src/DL_hw2.py
@@ -13,7 +13,7 @@ from torchvision import transforms, datasets
 N_EPOCHS = 10
 BATCH_SIZE_TRAIN = 100
 BATCH_SIZE_TEST = 100
-LR = 0.01
+LEARNING_RATE = 0.01
 
 
 #--- fixed constants ---
@@ -36,24 +36,29 @@ train_set = datasets.ImageFolder(DATA_DIR % 'train', transform=train_transform)
 dev_set   = datasets.ImageFolder(DATA_DIR % 'dev',   transform=test_transform)
 test_set  = datasets.ImageFolder(DATA_DIR % 'test',  transform=test_transform)
 
-
 # Create Pytorch data loaders
 train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=BATCH_SIZE_TEST, shuffle=False)
-
 
 #--- model ---
 class CNN(nn.Module):
     def __init__(self, num_classes=NUM_CLASSES):
         super(CNN, self).__init__()
-        # WRITE CODE HERE
-        pass
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, NUM_CLASSES)
 
     def forward(self, x):
-        # WRITE CODE HERE
-        pass
-
-
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 #--- set up ---
 if torch.cuda.is_available():
@@ -62,31 +67,39 @@ else:
     device = torch.device('cpu')
 
 model = CNN().to(device)
-
-# WRITE CODE HERE
-optimizer = None
-loss_function = None
-
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
+loss_function = nn.CrossEntropyLoss()
 
 #--- training ---
 for epoch in range(N_EPOCHS):
     train_loss = 0
     train_correct = 0
     total = 0
+
     for batch_num, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        # WRITE CODE HERE
 
-        print('Training: Epoch %d - Batch %d/%d: Loss: %.4f | Train Acc: %.3f%% (%d/%d)' % 
-              (epoch, batch_num, len(train_loader), train_loss / (batch_num + 1), 
-               100. * train_correct / total, train_correct, total))
-    
+        prediction = model(data)
+        output = loss_function(prediction, target)
+
+        train_loss += output.data.item()
+        _, predicted = torch.max(prediction, 1)
+        train_correct += (predicted == target).sum().item()
+        total += target.size(0)
+
+        output.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        print(f'Training: Epoch {epoch+1}/{N_EPOCHS} | Batch {batch_num}/{len(train_loader)} | Loss: %.4f | Train Acc: %.3f%% ({train_correct}/{total})' %
+              (train_loss / (batch_num + 1), 100. * train_correct / total),
+               end="\r",
+               flush=True)
+
     # WRITE CODE HERE
     # Please implement early stopping here.
     # You can try different versions, simplest way is to calculate the dev error and
     # compare this with the previous dev error, stopping if the error has grown.
-
-
 
 #--- test ---
 test_loss = 0
@@ -96,8 +109,18 @@ total = 0
 with torch.no_grad():
     for batch_num, (data, target) in enumerate(test_loader):
         data, target = data.to(device), target.to(device)
-        # WRITE CODE HERE
+
+        prediction = model(data)
+        _, predicted = torch.max(prediction.data, 1)
+
+        total += target.size(0)
+        test_correct += (predicted == target).sum()
+        test_loss += loss_function(prediction, target)
 
         print('Evaluating: Batch %d/%d: Loss: %.4f | Test Acc: %.3f%% (%d/%d)' % 
-              (batch_num, len(test_loader), test_loss / (batch_num + 1), 
-               100. * test_correct / total, test_correct, total))
+              (batch_num + 1, 
+               len(test_loader), 
+               test_loss / (batch_num + 1),
+               100. * test_correct / total, 
+               test_correct, 
+               total))
