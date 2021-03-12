0c4075e3321b4a53224515af63da03a45413880b
Ville Kopio <ville.kopio@kodan.fi>
2021-03-12 12:57:37 +0200

    Finalize assignment 2.

--- assignment_2/src/DL_hw2.py
+++ assignment_2/src/DL_hw2.py
@@ -19,7 +19,7 @@ from debug import *
 NUM_CLASSES = 24
 DATA_DIR = '../data/sign_mnist_%s'
 
-N_EPOCHS = 30
+N_EPOCHS = 10
 BATCH_SIZE = 100
 LEARNING_RATE = 0.0001
 
@@ -60,25 +60,25 @@ class CNN(nn.Module):
     def __init__(self, num_classes=NUM_CLASSES):
         super(CNN, self).__init__()
         self.seq1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=28, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=28),
+            nn.BatchNorm2d(num_features=32),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
         )
         self.seq2 = nn.Sequential(
-            nn.Conv2d(in_channels=28, out_channels=64, kernel_size=3),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=64),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(p=0.1),
         )
         self.seq3 = nn.Sequential(
             nn.Linear(in_features=64 * 6 * 6, out_features=512),
             nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=512, out_features=NUM_CLASSES)
+            nn.Linear(in_features=512, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=NUM_CLASSES)
         )
-        self.fc1 = nn.Linear(64 * 6 * 6, 120)
+        self.fc1 = nn.Linear(32 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, NUM_CLASSES)
 
@@ -87,10 +87,6 @@ class CNN(nn.Module):
         x = self.seq2(x)
         x = x.view(x.size(0), -1)
         x = self.seq3(x)
-        #x = x.view(-1, 64 * 6 * 6)
-        #x = F.relu(self.fc1(x))
-        #x = F.relu(self.fc2(x))
-        #x = self.fc3(x)
         x = F.log_softmax(x, dim=1)
         return x
 
@@ -99,7 +95,7 @@ train_loader = load_dataset(
     'train',
     shuffle=True,
     extra_transforms=[
-        AddGaussianNoise(0.1, 0.01),
+        #AddGaussianNoise(0.1, 0.1),
     ]
 )
 
